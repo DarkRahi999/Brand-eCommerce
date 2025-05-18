d4f8a5b4f81980a30bf57e2c7f6da8cc3eb3724b
Bisakto_Rahi <bisaktorahi007@gmail.com>
2025-05-18 14:40:00 +0600

    landing component create done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,16 +1,10 @@
+import LandingImg from '../components/LandingImg'
 import Footer from '../Layouts/Footer'
 export default function ContactPage() {
   return (
     <div>
       <div className=" MyWeb p-4">
-        <div className="landing">
-          <div className="Img">
-            <img src="/img/contact/contact.jpg" alt="Contact" />
-          </div>
-          <div className="tagline">
-           <h1 className="text-white text-3xl">CONTACT US</h1>
-          </div>
-        </div>
+        <LandingImg />
       </div>
       <Footer />
     </div>
